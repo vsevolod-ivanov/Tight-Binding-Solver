92919b5cfb452d6d14f7a79997d815573a7d79ee
Vsevolod Ivanov <vsevolod.m.ivanov@gmail.com>
2017-11-24 13:54:24 -0800

    Added first Hamiltonian function

---
+++ hamiltonian.h
@@ -0,0 +1,7 @@
+#ifndef _LAPACKE_EXAMPLE_AUX_
+#define _LAPACKE_EXAMPLE_AUX_
+
+
+lapack_complex_double graphene_H(lapack_int n, double* spin, double* vec, lapack_complex_double* coef);
+
+#endif /* _LAPACKE_EXAMPLE_AUX_*/
